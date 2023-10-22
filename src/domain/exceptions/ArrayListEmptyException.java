78ce758d9310131907bc2800f6e44f888183ac64
abelsouzacosta <abelsouzacosta@gmail.com>
2023-10-22 03:57:07 -0300

    [chore]: adding exception ArrayListEmptyException

--- src/domain/exceptions/ArrayListEmptyException.java
+++ src/domain/exceptions/ArrayListEmptyException.java
@@ -1,6 +1,7 @@
 package domain.exceptions;
 
 public class ArrayListEmptyException extends Exception {
+    // TODO: remove exception
     public ArrayListEmptyException(String message) {
         super(message);
     }
