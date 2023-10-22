11396dd4aec0b7430c56a085dba911d6416e1ed2
abelsouzacosta <abelsouzacosta@gmail.com>
2023-10-22 04:01:20 -0300

    [feat]: bootstrap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import UI.UI;
+import domain.exceptions.ArrayListFullException;
+
+import java.text.ParseException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            UI.bootstrap();
+        } catch (ArrayListFullException e) {
+            throw new RuntimeException(e.getMessage());
+        } catch (ParseException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
